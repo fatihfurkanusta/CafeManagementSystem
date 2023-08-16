e7e3c52ce409a229d53f3bcf103e1be857eda71e
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-08-16 14:18:27 +0300

    upload project

---
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,10 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+    public static final String SOMETHING_WENT_WRONG = "Something went Wrong.";
+    public static final String INVALID_DATA = "Invalid Data";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+    public static final String STORE_LOCATION = "C:\\Java Projects\\cafe-stored-file";
+
+}
